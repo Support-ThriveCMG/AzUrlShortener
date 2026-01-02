407f7e4dfac0ca7679779f4fb173990ab085dfac
Support-ThriveCMG <support@thrivecmg.com>
2026-01-02 13:04:33 -0800

    Add Azure Table Client to service configuration

--- src/FunctionsLight/Program.cs
+++ src/FunctionsLight/Program.cs
@@ -15,9 +15,14 @@
 // builder.Build().Run();
 
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(services =>
+    {
+        services.AddAzureTableClient("strTables");
+    })
     .Build();
 
 host.Run();
