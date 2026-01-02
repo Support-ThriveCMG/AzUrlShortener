07c3c6b2dce36737b3b9000e9311586c908c63ec
Support-ThriveCMG <support@thrivecmg.com>
2026-01-02 14:56:35 -0800

    Update Program.cs

--- src/FunctionsLight/Program.cs
+++ src/FunctionsLight/Program.cs
@@ -1,11 +1,18 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Azure.Data.Tables;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
     .ConfigureServices(services =>
     {
-        services.AddAzureTableClient("strTables");
+        services.AddSingleton<TableServiceClient>(sp =>
+        {
+            var connectionString =
+                Environment.GetEnvironmentVariable("strTables");
+
+            return new TableServiceClient(connectionString);
+        });
     })
     .Build();
 
