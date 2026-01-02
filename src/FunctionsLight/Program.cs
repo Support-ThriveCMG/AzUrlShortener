af9f23ac05ea19a79402cfb84738f6d955a6f30b
Support-ThriveCMG <support@thrivecmg.com>
2026-01-02 13:11:02 -0800

    Comment out Application Insights note in Program.cs

--- src/FunctionsLight/Program.cs
+++ src/FunctionsLight/Program.cs
@@ -7,8 +7,8 @@ builder.AddAzureTableClient("strTables");
 
 builder.ConfigureFunctionsWebApplication();
 
-Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
-    builder.Services
+// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
+builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
     .ConfigureFunctionsApplicationInsights();
 
