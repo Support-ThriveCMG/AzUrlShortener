0e2e5658b37619f0d3f758f6148b9df3fa223415
Support-ThriveCMG <support@thrivecmg.com>
2026-01-02 13:06:57 -0800

    Refactor Program.cs for Azure Functions setup

--- src/FunctionsLight/Program.cs
+++ src/FunctionsLight/Program.cs
@@ -1,28 +1,16 @@
-// using Microsoft.Azure.Functions.Worker.Builder;
-// using Microsoft.Extensions.Hosting;
-
-// var builder = FunctionsApplication.CreateBuilder(args);
-
-// builder.AddAzureTableClient("strTables");
-
-// builder.ConfigureFunctionsWebApplication();
-
-// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
-// builder.Services
-//     .AddApplicationInsightsTelemetryWorkerService()
-//     .ConfigureFunctionsApplicationInsights();
-
-// builder.Build().Run();
-
+using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 
-var host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults()
-    .ConfigureServices(services =>
-    {
-        services.AddAzureTableClient("strTables");
-    })
-    .Build();
+var builder = FunctionsApplication.CreateBuilder(args);
 
-host.Run();
+builder.AddAzureTableClient("strTables");
+
+builder.ConfigureFunctionsWebApplication();
+
+Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
+    builder.Services
+    .AddApplicationInsightsTelemetryWorkerService()
+    .ConfigureFunctionsApplicationInsights();
+
+builder.Build().Run();
+
