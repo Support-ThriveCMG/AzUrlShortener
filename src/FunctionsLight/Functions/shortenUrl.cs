883902a103cf98ccdb65af95e0469f2249690a7b
Support-ThriveCMG <support@thrivecmg.com>
2026-01-09 15:58:19 -0800

    Create shortenUrl.cs

---
+++ src/FunctionsLight/Functions/shortenUrl.cs
@@ -0,0 +1,57 @@
+using Cloud5mins.ShortenerTools.Core.Services;
+using Cloud5mins.ShortenerTools.Core.Service;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Text.Json;
+using Azure.Data.Tables;
+
+namespace Cloud5mins.ShortenerTools.Functions
+{
+    public class CreateShortUrl
+    {
+        private readonly ILogger _logger;
+        private readonly TableServiceClient _tblClient;
+
+        public CreateShortUrl(ILoggerFactory loggerFactory, TableServiceClient tblClient)
+        {
+            _logger = loggerFactory.CreateLogger<CreateShortUrl>();
+            _tblClient = tblClient;
+        }
+
+        [Function("CreateShortUrl")]
+        public async Task<HttpResponseData> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req)
+        {
+            var body = await new StreamReader(req.Body).ReadToEndAsync();
+            var request = JsonSerializer.Deserialize<CreateShortUrlRequest>(body);
+
+            if (string.IsNullOrWhiteSpace(request?.Url))
+            {
+                var bad = req.CreateResponse(HttpStatusCode.BadRequest);
+                await bad.WriteStringAsync("Url is required");
+                return bad;
+            }
+
+            var shortCode = Guid.NewGuid().ToString("N")[..6];
+
+            var urlService = new UrlServices(
+                _logger,
+                new AzStrorageTablesService(_tblClient));
+
+            await urlService.Create(shortCode, request.Url);
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            await response.WriteAsJsonAsync(new
+            {
+                shortCode,
+                shortUrl = $"https://azurlshortener1.azurewebsites.net/api/{shortCode}"
+            });
+
+            return response;
+        }
+    }
+
+    public record CreateShortUrlRequest(string Url);
+}
